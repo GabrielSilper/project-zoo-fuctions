eb1d1fe39e0e5ea87a601664dbcac0b66f57b95e
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 13:18:40 -0400

    Requisito 8 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsQtt = { child: 0, adult: 0, senior: 0 };
+  return entrants.reduce((acum, current) => {
+    if (current.age < 18) {
+      entrantsQtt.child += 1;
+    } else if (current.age >= 18 && current.age < 50) {
+      entrantsQtt.adult += 1;
+    } else if (current.age >= 50) {
+      entrantsQtt.senior += 1;
+    }
+    return entrantsQtt;
+  }, entrantsQtt);
 }
 
+const { prices } = data;
+
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof entrants !== 'object' || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrantsQtt = countEntrants(entrants);
+  const total = Object.entries(entrantsQtt);
+  return total.map((elem) => prices[elem[0]] * elem[1])
+    .reduce((acum, current) => acum + current, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
