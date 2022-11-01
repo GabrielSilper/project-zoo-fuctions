2f6eb68bdfa785c68701c6dcd0b8f00c586d2316
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 10:49:21 -0400

    Requisito 2 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const { species } = data;
+  const { residents } = species.find((elem) => elem.name === animal);
+  return residents.every((elem) => elem.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
