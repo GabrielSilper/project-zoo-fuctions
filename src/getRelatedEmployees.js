9cc3ba22345231481528929b903a74f4be64e61e
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 11:26:46 -0400

    Requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.map((employee) => employee.managers)
+    .reduce((acum, current) => acum.concat(current), []).some((manager) => manager === id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((elem) => elem.managers.some((manager) => manager === managerId))
+    .map((elem) => `${elem.firstName} ${elem.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
