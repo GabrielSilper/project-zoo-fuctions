2e9f816131982b4779a518e65de636a3757fe23d
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 11:32:31 -0400

    Requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const animals = {};
+    return species.reduce((acum, current) => {
+      animals[current.name] = current.residents.length;
+      return animals;
+    }, {});
+  }
+  const { specie, sex } = animal;
+  if (sex === undefined) {
+    return species.find((elem) => elem.name === specie).residents.length;
+  }
+  return species.find((elem) => elem.name === specie).residents
+    .filter((elem) => elem.sex === sex).length;
 }
 
 module.exports = countAnimals;
