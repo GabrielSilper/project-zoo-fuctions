7efc8f159ac466429bf0db76ff5cd9a17870ad1d
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 19:46:19 -0400

    Requisito 11 ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function getAllEmployeesCoverage() {
+  return employees.map((employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    return {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: responsibleFor.map((elem) => species
+        .find((specie) => specie.id === elem).name),
+      locations: responsibleFor.map((elem) => species
+        .find((specie) => specie.id === elem).location),
+    };
+  });
 }
+
+function getEmployeeCoverageByName(name) {
+  const verify = getAllEmployeesCoverage().some((elem) => elem.fullName.includes(name));
+  if (verify === false) {
+    throw new Error('Informações inválidas');
+  }
+  return getAllEmployeesCoverage().find((elem) => elem.fullName.includes(name));
+}
+
+function getEmployeeCoverageByID(id) {
+  const verify = getAllEmployeesCoverage().some((elem) => elem.id === id);
+  if (verify === false) {
+    throw new Error('Informações inválidas');
+  }
+  return getAllEmployeesCoverage().find((elem) => elem.id === id);
+}
+
+function getEmployeesCoverage(object) {
+  if (typeof object === 'object') {
+    const { name, id } = object;
+    if (name !== undefined) {
+      return getEmployeeCoverageByName(name);
+    }
+    if (id !== undefined) {
+      return getEmployeeCoverageByID(id);
+    }
+  }
+  return getAllEmployeesCoverage();
+}
+
+console.log(getEmployeeCoverageByID('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = getEmployeesCoverage;
