9e60add33bb5aa393aea15e824f3757df1f0f477
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 18:19:18 -0400

    Requisito 10 ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const animal = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const olderAnimal = species.find((specie) => specie.id === animal).residents
+    .reduce((acum, current) => (acum.age > current.age ? acum : current));
+  const { name, sex, age } = olderAnimal;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
