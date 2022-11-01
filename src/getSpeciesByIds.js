0ac0427e54a3a37510dc586d4a05bf7c8244c773
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-01 10:17:50 -0400

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const { species } = data;
+  return species.reduce((acum, curr) => {
+    ids.forEach((id) => {
+      if (curr.id === id) {
+        acum.push(curr);
+      }
+    });
+    return acum;
+  }, []);
 }
 
 module.exports = getSpeciesByIds;
